4b5776611725830a8960e21c44ce574d2dc37149
Pavel Lukoyanov <148583866+complagaet@users.noreply.github.com>
2024-04-06 19:48:49 +0500

    hw1

--- HW-1/hard.playground/Contents.swift
+++ HW-1/hard.playground/Contents.swift
@@ -1,1 +1,70 @@
+//@@@@@@@@@@@@@@@@@@@@@
+//       Task 1
+//@@@@@@@@@@@@@@@@@@@@@
 
+func RomanNumeral(_ roman: String) -> Int {
+    let values: [Character: Int] = ["I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000]
+    var result = 0
+    var prevValue = 0
+    
+    for char in roman.reversed() {
+        if let value = values[char] {
+            if value < prevValue {
+                result -= value
+            } else {
+                result += value
+            }
+            prevValue = value
+        }
+    }
+    
+    return result
+}
+
+print("------ Task 1 ------")
+print(RomanNumeral("MMMCMV"))
+print(RomanNumeral("VII"))
+
+//@@@@@@@@@@@@@@@@@@@@@
+//       Task 2
+//@@@@@@@@@@@@@@@@@@@@@
+
+func caesarCipher(_ str: String, _ offset: Int) -> String {
+    let alphabet = Array("abcdefghijklmnopqrstuvwxyz")
+    var result = ""
+    for i in str {
+        if alphabet.contains(i.lowercased()) {
+            let letterIndexInAlphabet = alphabet.firstIndex(of: Character(i.lowercased()))!
+            var letterIndexOffset = letterIndexInAlphabet + offset
+            
+            while letterIndexOffset > alphabet.count - 1 {
+                letterIndexOffset -= alphabet.count
+            }
+            result += (i == Character(i.uppercased())) ? String(alphabet[letterIndexOffset]).uppercased() : String(alphabet[letterIndexOffset])
+        }
+        else {
+            result += String(i)
+        }
+    }
+    return result
+}
+
+print("\n------ Task 2 ------")
+print(caesarCipher("middle-Outz", 2))
+
+//@@@@@@@@@@@@@@@@@@@@@
+//       Task 3
+//@@@@@@@@@@@@@@@@@@@@@
+
+func sorting(_ input: String) -> String {
+    let uniqueCharacters = Set(input)
+    let sortedUniqueCharacters = uniqueCharacters.sorted()
+    let sortedString = String(sortedUniqueCharacters)
+    return sortedString
+}
+
+print("\n------ Task 3 ------")
+print(sorting("eA2a1E"))  // "aAeE12"
+print(sorting("Re4r"))    // "erR4"
+print(sorting("6jnM31Q")) // "jMnQ136"
+print(sorting("846ZIbo")) // "bIoZ468"
--- HW-1/middle.playground/Contents.swift
+++ HW-1/middle.playground/Contents.swift
@@ -1,3 +1,7 @@
+//@@@@@@@@@@@@@@@@@@@@@
+//       Task 1
+//@@@@@@@@@@@@@@@@@@@@@
+
 func toCamelCase(_ str: String) -> String {
     var result = ""
     for i in str.split(separator: "_") {
@@ -22,5 +26,91 @@ func toSnakeCase(_ str: String) -> String {
     return result
 }
 
+print("------ Task 1 ------")
 print(toCamelCase("hello_ogurec_kak_dela"))
 print(toSnakeCase("pelmeniAreBetterThanArbuz"))
+
+//@@@@@@@@@@@@@@@@@@@@@
+//       Task 2
+//@@@@@@@@@@@@@@@@@@@@@
+
+func lcmOfArray(_ array: [Int]) -> Int {
+    if array.count < 3 {
+        return -1
+    }
+    
+    let firstNumber = array.first!
+    
+    func gcd(_ a: Int, _ b: Int) -> Int {
+        var x = a
+        var y = b
+        while y != 0 {
+            let temp = y
+            y = x % y
+            x = temp
+        }
+        return x
+    }
+    
+    func lcm(_ a: Int, _ b: Int) -> Int {
+        return a * b / gcd(a, b)
+    }
+    
+    var result = firstNumber
+    for number in array {
+        result = lcm(result, number)
+    }
+    
+    return result
+}
+
+print("\n------ Task 2 ------")
+print(lcmOfArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+print(lcmOfArray([13, 6, 17, 18, 19, 20, 37]))
+print(lcmOfArray([44, 64, 12, 17, 65]))
+
+//@@@@@@@@@@@@@@@@@@@@@
+//       Task 3
+//@@@@@@@@@@@@@@@@@@@@@
+
+func firstVowel(_ str: String) -> Int {
+    let vowels = "aeiouy"
+    var count = 0
+    for i in str.lowercased() {
+        if vowels.contains(String(i)) {
+            return count
+        }
+        count += 1
+    }
+    return -1
+}
+
+print("\n------ Task 3 ------")
+print(firstVowel("apple"))
+print(firstVowel("hello"))
+print(firstVowel("STRAWBERRY"))
+print(firstVowel("pInEaPPLe"))
+
+//@@@@@@@@@@@@@@@@@@@@@
+//       Task 4
+//@@@@@@@@@@@@@@@@@@@@@
+
+func numInStr(_ array: [String]) -> [String] {
+    let digits = "0123456789"
+    var result: [String] = []
+    for i in array {
+        for j in i {
+            if digits.contains(String(j)) {
+                result.append(i)
+                break
+            }
+        }
+    }
+    return result
+}
+
+print("\n------ Task 4 ------")
+print(numInStr(["1a", "a", "2b", "b"]))
+print(numInStr(["abc", "abc10"]))
+print(numInStr(["abc", "ab10c", "a10bc", "bcd"]))
+print(numInStr(["this is a test", "test1"]))
